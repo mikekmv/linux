8e2b077eb440bf610a3fc50b558a8f17335dcf0a
Mike <mikekmv@users.noreply.github.com>
2015-04-14 09:08:39 +0300

    Added 352.8kHz and 384kHz samplerates

--- sound/soc/bcm/bcm2835-i2s.c
+++ sound/soc/bcm/bcm2835-i2s.c
@@ -712,14 +712,14 @@ static struct snd_soc_dai_driver bcm2835_i2s_dai = {
 	.playback = {
 		.channels_min = 2,
 		.channels_max = 2,
-		.rates =	SNDRV_PCM_RATE_8000_192000,
+		.rates =	SNDRV_PCM_RATE_8000_384000,
 		.formats =	SNDRV_PCM_FMTBIT_S16_LE
 				| SNDRV_PCM_FMTBIT_S32_LE
 		},
 	.capture = {
 		.channels_min = 2,
 		.channels_max = 2,
-		.rates =	SNDRV_PCM_RATE_8000_192000,
+		.rates =	SNDRV_PCM_RATE_8000_384000,
 		.formats =	SNDRV_PCM_FMTBIT_S16_LE
 				| SNDRV_PCM_FMTBIT_S32_LE
 		},
